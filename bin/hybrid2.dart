57955574abc1afe70ffb3954c1377a2901104914
murt0021 <murt0021@algonquinlive.com>
2023-09-28 16:38:55 -0400

    varible types

--- bin/hybrid2.dart
+++ bin/hybrid2.dart
@@ -2,7 +2,8 @@ import 'package:hybrid2/hybrid2.dart';
 
 void main(List<String> arguments) {
   for (var name in arguments) {
-    var capitalName = name[0].toUpperCase() + name.substring(1).toLowerCase();
+    String capitalName =
+        name[0].toUpperCase() + name.substring(1).toLowerCase();
     var lucky = Lucky();
     print('Hello $capitalName. Your lucky number is ${lucky.number}');
   }
--- lib/hybrid2.dart
+++ lib/hybrid2.dart
@@ -5,8 +5,8 @@ class Lucky {
   late int number;
 
   Lucky() {
-    var min = int.parse(Platform.environment['MIN'] ?? '10');
-    var max = int.parse(Platform.environment['MAX'] ?? '100');
+    int min = int.parse(Platform.environment['MIN'] ?? '10');
+    int max = int.parse(Platform.environment['MAX'] ?? '100');
     var random = Random();
     number = min + random.nextInt(max - min + 1);
   }
